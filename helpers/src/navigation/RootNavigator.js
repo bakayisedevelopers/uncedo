aa6becd5b8d5ab0570afed347c3e1a28f35aa12a
Bakayise Developers <bakayise.developers@gmail.com>
2026-06-27 13:40:39 +0200

    Fix helper queue retries and offer overlay UX

--- helpers/src/navigation/RootNavigator.js
+++ helpers/src/navigation/RootNavigator.js
@@ -304,7 +304,7 @@ export function RootNavigator() {
           </SafeAreaView>
         ) : null}
 
-        <HelperOfferOverlay />
+        <HelperOfferOverlay bottomSafeInset={bottomSystemInset} />
       </View>
     </View>
   );
